584fc4e1404c3076fa308b2589c36d78a3a00a24
Mark <155267500+rqms40@users.noreply.github.com>
2024-10-18 23:34:00 +0800

    chore: remove unnecessary code and animation

--- src/App.jsx
+++ src/App.jsx
@@ -3,43 +3,15 @@ import Navbar from "./components/navbar/Navbar";
 import Footer from "./components/footer/Footer";
 
 const App = () => {
-  const colors = [
-    "text-violet-600",
-    "text-red-600",
-    "text-blue-600",
-    "text-green-600",
-  ];
-
-  const hoverColors = [
-    "hover:text-violet-800",
-    "hover:text-red-800",
-    "hover:text-blue-800",
-    "hover:text-green-800",
-  ];
-
-  const [colorIndex, setColorIndex] = useState(0);
-
-  useEffect(() => {
-    const interval = setInterval(() => {
-      setColorIndex((prevIndex) => (prevIndex + 1) % colors.length);
-    }, 1000);
-
-    return () => clearInterval(interval);
-  }, [colors]);
-
   return (
     <>
       <Navbar />
 
-      <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100 animate-fadeIn">
-        <h1
-          className={`text-4xl font-extrabold ${colors[colorIndex]} ${hoverColors[colorIndex]} transition duration-300 ease-in-out mb-4 animate-bounce`}
-        >
+      <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100 text-center">
+        <h1 className="text-4xl font-extrabold text-custom-violet hover:text-custom-violet-dark mb-4">
           LE 1 - Creative Output
         </h1>
-        <h2
-          className={`text-2xl font-semibold ${colors[colorIndex]} ${hoverColors[colorIndex]} transition duration-300 ease-in-out animate-pulse`}
-        >
+        <h2 className="text-2xl font-semibold text-custom-violet hover:text-custom-violet-dark">
           Group 3
         </h2>
       </div>
