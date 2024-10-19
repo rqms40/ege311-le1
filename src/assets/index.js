d8bed851587da151f1bed0d2c4f134e2a3d4e75f
Mark <155267500+rqms40@users.noreply.github.com>
2024-10-19 18:08:08 +0800

    chore: add comments to organize import statements

--- src/assets/index.js
+++ src/assets/index.js
@@ -1,5 +1,14 @@
 import logo from "./logo.svg";
 
+// Hero
 import heroBackground from "./hero/hero-background.png";
+
+// Reproduction
+
+// Hygiene
+
+// Adolescents
+
+// About
 
 export { logo, heroBackground };
