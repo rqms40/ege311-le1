0bc615f23b1509822818af5700d22f8de03dc71e
Mark <155267500+rqms40@users.noreply.github.com>
2024-10-26 03:10:43 +0800

    feat: assets for adolescents section

--- src/assets/index.js
+++ src/assets/index.js
@@ -10,7 +10,26 @@ import sexualHygieneImage from "./hygiene/sexual-hygiene.jpg";
 import cardIcon from "./hygiene/cardIcon.svg";
 
 // Adolescents
+import adolescents from "./adolescents/image-1.png";
+import riskyBehavior1 from "./adolescents/risky-behavior-1.png";
+import riskyBehavior2 from "./adolescents/risky-behavior-2.png";
+import riskyBehavior3 from "./adolescents/risky-behavior-3.png";
+import riskyBehavior4 from "./adolescents/risky-behavior-4.png";
+import riskyBehavior5 from "./adolescents/risky-behavior-5.png";
+import riskyBehavior6 from "./adolescents/risky-behavior-6.png";
 
 // About
 
-export { logo, heroBackground, sexualHygieneImage, cardIcon };
+export {
+  logo,
+  heroBackground,
+  adolescents,
+  riskyBehavior1,
+  riskyBehavior2,
+  riskyBehavior3,
+  riskyBehavior4,
+  riskyBehavior5,
+  riskyBehavior6,
+  sexualHygieneImage,
+  cardIcon,
+};
