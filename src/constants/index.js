3700bb769756effa9475b7204c33e0fa908e4c50
Mark <155267500+rqms40@users.noreply.github.com>
2024-10-26 03:11:10 +0800

    feat: add constants for adolescents section

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,3 +1,12 @@
+import {
+  riskyBehavior1,
+  riskyBehavior2,
+  riskyBehavior3,
+  riskyBehavior4,
+  riskyBehavior5,
+  riskyBehavior6,
+} from "../assets";
+
 export const navLinksData = [
   {
     _id: 1001,
@@ -23,5 +32,50 @@ export const navLinksData = [
     _id: 1005,
     title: "About",
     link: "about",
+  },
+];
+
+export const riskyBehaviors = [
+  {
+    id: 0,
+    title: "Sexual Experience",
+    description:
+      "Any form of acts involving sexual intimacy  between individuals. It can vary widely, from physical touch and affection to more explicit sexual acts, depending on personal boundaries and comfort levels.",
+    imageUrl: riskyBehavior1,
+  },
+  {
+    id: 1,
+    title: "Smoking",
+    description:
+      "The practice of inhaling smoke from burning plant material. Nicotine works on your brain to create a relaxing, pleasurable feeling that makes it tough to quit. But smoking tobacco puts you at risk for cancer, stroke, etc.",
+    imageUrl: riskyBehavior2,
+  },
+  {
+    id: 2,
+    title: "Teen Motherhood",
+    description:
+      "Refers to the experience of a young woman, typically between the ages of 13 and 19, becoming a mother. It often presents unique challenges, such as balancing education, financial stability, and childcare responsibilities.",
+    imageUrl: riskyBehavior3,
+  },
+  {
+    id: 3,
+    title: "Alcohol Use",
+    description:
+      "The consumption of beverages containing ethanol, which can alter mood, behavior, and cognitive functions. Regular or excessive alcohol consumption can lead to dependency, health issues, etc.",
+    imageUrl: riskyBehavior4,
+  },
+  {
+    id: 4,
+    title: "Drug Use",
+    description:
+      "Refers to the intake of substances, legal or illegal, that can affect the body’s central nervous system and alter physical or mental states. Recreational drug use may lead to addiction, health complications, etc.",
+    imageUrl: riskyBehavior5,
+  },
+  {
+    id: 5,
+    title: "Sex for Payment",
+    description:
+      "Transactional sex or sex work, involves exchanging sexual acts for money or goods. This activity may be voluntary or coerced, depending on personal or external circumstances. The practice can carry risks.",
+    imageUrl: riskyBehavior6,
   },
 ];
