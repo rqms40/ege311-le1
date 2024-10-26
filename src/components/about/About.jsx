e09e45c42424b3a92799f27c362de5c1f3afafdc
Mark David Prado <155267500+kiritoS40@users.noreply.github.com>
2024-10-26 23:48:40 +0800

    feat(nav): add link for about section

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -5,7 +5,7 @@ import AboutCard from "../reproduction/AboutCard";
 
 const About = () => {
   return (
-    <SubsectionWrapper className="py-6 md:py-6 lg:py-20 px-0 md:px-0">
+    <SubsectionWrapper id="about" className="pt-16 md:pt-24 lg:pt-28 px-0 md:px-0">
       <h1 className="font-bold text-2xl md:text-4xl lg:text-5xl text-center">
         ABOUT US
       </h1>
